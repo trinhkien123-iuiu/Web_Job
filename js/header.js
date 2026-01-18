b39bae84dd92060124ebf47f7333ab524af272de
Trịnh Kiên <trinhkien1211@gmail.com>
2026-01-19 01:16:03 +0700

    update 19/01

--- js/header.js
+++ js/header.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const menuNav = header.querySelector('.menu-nav').querySelectorAll('a');
 
       switch (window.location.pathname) {
-        case '/html/index.html': menuNav[0].style.color = 'var(--main-text-click)'; break;
+        case '/html/Desktop_1.html': menuNav[0].style.color = 'var(--main-text-click)'; break;
         case "/html/product.html": menuNav[1].style.color = 'var(--main-text-click)'; break;
         case '/html/Desktop_3.html': menuNav[2].style.color = 'var(--main-text-click)'; break;
         case '/html/Desktop_4.html': menuNav[3].style.color = 'var(--main-text-click)'; break;
