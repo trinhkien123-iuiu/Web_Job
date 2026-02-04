766c70b65c2c2192d1fdf5aac1ec5d5f573f35db
vhieu2k5 <vhieeu2005@gmail.com>
2026-02-04 18:30:25 +0700

    add Loader

--- js/header.js
+++ js/header.js
@@ -1,13 +1,25 @@
-var keyword = '';
+var keyword = ''; 
+
+//Phan nay cho loader
+function hideLoader() {
+    const loader = document.getElementById('loader');
+   if (loader && loader.style.top !== '-2000px')
+     loader.style.top = '-2000px';
+     document.body.style.overflow = 'auto';
+   
+}
+window.addEventListener('load', hideLoader);
+
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector('#site-header[data-include]');
   if (!header) return;
   fetch(header.getAttribute("data-include"))
     .then(r => r.text())
     .then(html => {
+    
       header.innerHTML = html;
       const menuNav = header.querySelector('.menu-nav').querySelectorAll('a');
-
+    
       switch (window.location.pathname) {
         case '/html/index.html': menuNav[0].style.color = 'var(--main-text-click)'; break;
         case "/html/product.html": menuNav[1].style.color = 'var(--main-text-click)'; break;
@@ -16,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
         case '/html/Desktop_5.html': menuNav[4].style.color = 'var(--main-text-click)'; break;
         default: break;
       }
-
+     
       keyword = document.getElementById('search-input');
       var isTyping = false;
       keyword.addEventListener('keydown', function(event) {
@@ -28,11 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
               window.location.href = `/html/product.html?search=${encodeURIComponent(keywordValue)}`;
             }
         }
-    })
-    .catch(console.error);
-  
-
-
+      
+      });
+   setTimeout(hideLoader, 2000);
   }
-    )
+    ).catch(err => {
+      console.error(err);
+      hideLoader();
+    });
 });
